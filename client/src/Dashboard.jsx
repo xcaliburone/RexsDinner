210f5823698aa5b2492c54bd5b9d6f9432141e57
adhimrahman <adhimrahaman@gmail.com>
2024-05-23 15:34:00 +0800

    fix: dashboard card

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+// import React from 'react';
 
 function Dashboard() {
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <p>Selamat datang di dashboard!</p>
-        </div>
+        <>
+            <div className="card">
+                1
+            </div>
+        </>
     );
 }
 
