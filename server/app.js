facc9f8c644d9cbcf900ec4c1686e1794374604b
adhimrahman <adhimrahaman@gmail.com>
2024-05-26 16:56:02 +0800

    fix: complete-order

--- server/app.js
+++ server/app.js
@@ -3,8 +3,10 @@ const mysql = require('mysql')
 const cors = require('cors')
 const path = require('path')
 const { format } = require('date-fns');
+const bodyParser = require('body-parser');
 
 const app = express()
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
@@ -12,8 +14,8 @@ app.use(express.json());
 const port = 3032;
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // Sesuaikan dengan alamat frontend Anda
-    optionsSuccessStatus: 200 // beberapa legacy browsers (IE11, beberapa versi Chrome / Firefox...) tidak mengikuti 204
+    origin: 'http://localhost:5173', // Sesuaikan dengan alamat frontend
+    optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
@@ -29,6 +31,8 @@ connection.connect((err) => {
     if (err) throw err;
     console.log('Connected to MySQL Rex`s Dinner Database');
 });
+
+const { generateOrderId, generateOrderDetailsId } = require('./utils/generateID')
 
 app.get('/', (req, res) => {
     return res.redirect('/');
@@ -88,7 +92,6 @@ app.get('/menu-ingredients/:menu_id', (req, res) => {
 app.post('/create-order/:employeeId', (req, res) => {
     const { status, items, customer_name } = req.body;
     const { employeeId } = req.params;
-
     const orderTime = format(new Date(), 'yyyy-MM-dd HH:mm:ss');
 
     console.log('Received data:', { status, items, customer_name, employeeId, orderTime });
@@ -97,78 +100,98 @@ app.post('/create-order/:employeeId', (req, res) => {
         return res.status(400).send('Invalid data provided');
     }
 
+    const orderStatus = 'processing';
+
     try {
-        createOrder(employeeId, orderTime, status, items, customer_name, res);
+        createOrder(employeeId, orderTime, status, orderStatus, items, customer_name, res);
     } catch (error) {
         console.error('Error creating order:', error);
         return res.status(500).send('Internal Server Error');
     }
 });
 
-const createOrder = (employeeId, orderTime, status, items, customer_name, res) => {
-    connection.beginTransaction(err => {
-        if (err) {
-            console.error('Error starting transaction:', err);
-            return res.status(500).send('Internal Server Error');
-        }
+const createOrder = async (employeeId, orderTime, status, orderStatus, items, customer_name, res) => {
+    try {
+        const orderId = await generateOrderId();
+        const orderDetailsId = await generateOrderDetailsId();
 
-        const queryOrder = 'INSERT INTO orders (employee_id, order_time, status, customer_name, total_price) VALUES (?, ?, ?, ?, 0)';
-        connection.query(queryOrder, [employeeId, orderTime, status, customer_name], (err, result) => {
+        console.log("order id :", orderId);
+        console.log("order details id :", orderDetailsId);
+
+        connection.beginTransaction(async (err) => {
             if (err) {
-                console.error('Error inserting order:', err);
-                connection.rollback(() => res.status(500).send('Internal Server Error'));
-                return;
+                console.error('Error starting transaction:', err);
+                return res.status(500).send('Internal Server Error');
             }
 
-            const orderId = result.insertId;
-            const orderDetailsQuery = 'INSERT INTO order_details (order_id, menu_id, quantity, price) VALUES ?';
-            const orderDetails = items.map(item => [orderId, item.menu_id, item.quantity, item.price * item.quantity]);
-
-            connection.query(orderDetailsQuery, [orderDetails], err => {
+            const queryOrder = 'INSERT INTO orders (id, employee_id, customer_name, order_time, status, total_price) VALUES (?, ?, ?, ?, ?, 0, ?)';
+            connection.query(queryOrder, [orderId, employeeId, customer_name, orderTime, status, orderStatus], async (err, result) => {
                 if (err) {
-                    console.error('Error inserting order details:', err);
+                    console.error('Error inserting order:', err);
                     connection.rollback(() => res.status(500).send('Internal Server Error'));
                     return;
                 }
 
-                const updateTotalPriceQuery = 'UPDATE orders SET total_price = (SELECT SUM(price) FROM order_details WHERE order_id = ?) WHERE id = ?';
-                connection.query(updateTotalPriceQuery, [orderId, orderId], err => {
+                const orderDetailsQuery = 'INSERT INTO order_details (id, order_id, menu_id, quantity, price) VALUES ?';
+                const orderDetails = items.map(item => [orderDetailsId, orderId, item.menu_id, item.quantity, item.price * item.quantity]);
+
+                connection.query(orderDetailsQuery, [orderDetails], async (err) => {
                     if (err) {
-                        console.error('Error updating total price:', err);
+                        console.error('Error inserting order details:', err);
                         connection.rollback(() => res.status(500).send('Internal Server Error'));
                         return;
                     }
 
-                    connection.commit(err => {
+                    const updateTotalPriceQuery = 'UPDATE orders SET total_price = (SELECT SUM(price) FROM order_details WHERE order_id = ?) WHERE id = ?';
+                    connection.query(updateTotalPriceQuery, [orderId, orderId], async (err) => {
                         if (err) {
-                            console.error('Error committing transaction:', err);
+                            console.error('Error updating total price:', err);
                             connection.rollback(() => res.status(500).send('Internal Server Error'));
                             return;
                         }
 
-                        res.send({ success: true, message: 'Order created successfully' });
+                        // Delay selama 1 menit sebelum menyelesaikan transaksi
+                        setTimeout(() => {
+                            connection.commit(async (err) => {
+                                if (err) {
+                                    console.error('Error committing transaction:', err);
+                                    connection.rollback(() => res.status(500).send('Internal Server Error'));
+                                    return;
+                                }
+
+                                res.send({ success: true, message: 'Order created successfully' });
+                            });
+                        }, 60000); // 1 menit dalam milidetik
                     });
                 });
             });
         });
-    });
+    } catch (error) {
+        console.error('Error creating order:', error);
+        return res.status(500).send('Internal Server Error');
+    }
 };
 
 
 app.get('/orders', (req, res) => {
-    const query = 'SELECT * FROM orders WHERE status IN ("pending", "in-progress")';
+    const query = 'SELECT * FROM orders';
     connection.query(query, (err, results) => {
         if (err) return res.status(500).send(err);
         res.send(results);
     });
 });
 
-app.post('/complete-order', (req, res) => {
-    const { orderId } = req.body;
-    const query = 'UPDATE orders SET status = "completed" WHERE id = ?';
-    
-    connection.query(query, [orderId], (err) => {
-        if (err) return res.status(500).send(err);
+app.put('/complete-order/:orderId', (req, res) => {
+    const { orderId } = req.params;
+
+    const query = 'UPDATE orders SET order_status = ? WHERE id = ?';
+
+    connection.query(query, ['completed', orderId], (err, results) => {
+        if (err) {
+            console.error('Error completing order:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+
         res.send({ success: true, message: 'Order completed successfully' });
     });
 });
