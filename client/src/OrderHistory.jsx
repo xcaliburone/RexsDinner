ec8eddbde32a4162a78cc4f9743408af33d90eb7
adhimrahman <adhimrahaman@gmail.com>
2024-05-31 19:09:37 +0800

    fix: order_id proptypes

--- client/src/OrderHistory.jsx
+++ client/src/OrderHistory.jsx
@@ -41,7 +41,7 @@ function OrderHistory({ orders }) {
 OrderHistory.propTypes = {
     orders: PropTypes.arrayOf(
         PropTypes.shape({
-            order_id: PropTypes.number.isRequired,
+            order_id: PropTypes.string.isRequired,
             customer_name: PropTypes.string.isRequired,
             status: PropTypes.string.isRequired,
             order_status: PropTypes.string.isRequired,
